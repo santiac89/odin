3c60369b57db96c7b6568609bdefe227b61a9c2c
Santiago Carullo <scarullo@elementum.com>
2018-03-04 11:29:03 -0300

    Add torrents resume

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
-const config = require('config')
-const { createServer } = require('http')
-const { fork } = require('child_process')
-const log = require('debug')('odin:main')
-const api = require('./api')
-const dropboxWatcher = require('./lib/dropbox_watcher')
-const library = require('./lib/library')
+const config = require('config');
+const { createServer } = require('http');
+const { fork } = require('child_process');
+const log = require('debug')('odin:main');
+const api = require('./api');
+const dropboxWatcher = require('./lib/dropbox_watcher');
+const torrentManager = require('./lib/torrent_manager');
+const library = require('./lib/library');
 
-let server = createServer(api)
+let server = createServer(api);
 
-server.listen(config.api.port, () => log('odin listening to you on port 3000!'))
+server.listen(config.api.port, () => log('odin listening to you on port 3000!'));
 
 library.reload();
 dropboxWatcher.start();
+torrentManager.resume();
